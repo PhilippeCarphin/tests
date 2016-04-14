92ecfff13b998c70198fdfc26ed5791649874e24
PewMcDaddy <phil103@hotmail.com>
2016-04-14 18:32:20 +0000

    Comments about stripLastElem.c, it doen't work but I know why.  No time to fix it because I'm at work

--- stripLastElem.c
+++ stripLastElem.c
@@ -21,7 +21,7 @@ int main ( int argc , char ** argv ) {
    char * token = strtok(path,"/");
    while ( token != NULL ){
       printf ( "token = %s\n", token);
-      token = strtok(path,"/");
+      token = strtok(NULL,"/");
    }
 
    if ( strcmp (path, "/first/second/third" ) == 0 ||
@@ -31,6 +31,18 @@ int main ( int argc , char ** argv ) {
       printf( "Test failed, ");
    }
    printf( "stripped path : %s\n", path );
+
+   /* I had fun doing this but I ended up finding out that there was already a
+    * thing called _nodeDataPtr->container
+    *
+    * Possibly after putting lastSlash = '\0', I could strdup the string and
+    * free the initial one.
+    *
+    * Also note the strdup at the definition of path.  Since the string litteral
+    * gets put in the .data segment, it is un modifiable.  And as we know,
+    * strtok changes the thing you use it on.
+    *
+    * Also, the test fails because the slashes were replaced by NULL == '\0'*/
 
    return 0;
 }

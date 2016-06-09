62946c47506ccb3258c40c370917bc9c07c561b3
Philippe Carphin <philippe.carphin@canada.ca>
2016-06-09 19:22:10 +0000

    Working linewrapper, I don't like how I have to do strlen() at the start

--- lineWrapper.c
+++ lineWrapper.c
@@ -17,25 +17,36 @@ int isDelim(char c){
 int main ( int argc , char ** argv ) {
 
    if( argc <= 1 ) exit(1);
+
+   if ( strlen(argv[1]) <= 15 ) {
+      puts ( argv[1] );
+      exit(0);
+   }
+
    char * input = strdup(argv[1]);
 
    char * current = NULL, * previous = NULL;
 
    int chars = 0;
+   int totalChars = 0;
 
    /* Find last word break with fewer than 80 chars */
 
    current = input;
    while( *current != '\0' ){
-      while ( chars <= 15){
+      while ( chars <= 15 && *current != '\0'){
          previous = current - 1;
          while ( !isDelim( *current ) ) ++current, ++chars;
-         if( *current != '\0' ) ++current;
+         if( *current != '\0' )
+            ++current;
+         else
+            goto done;
       }
       *(previous) = '\n';
       chars = 0;
    }
 
+done:
    puts(input);
    free(input);
 

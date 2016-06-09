42522f6fed8d9f6fa6533e4ad3b29b692df251d1
Philippe Carphin <philippe.carphin@canada.ca>
2016-06-09 23:22:00 +0000

    linewrapper with two algorithms

--- lineWrapper.c
+++ lineWrapper.c
@@ -14,41 +14,75 @@ int isDelim(char c){
    }
 }
 
+int printSection(const char * start, const char * end){
+   const char * p = start;
+   while ( p <= end ) putchar(*p++);
+}
+
 int main ( int argc , char ** argv ) {
 
-   if( argc <= 1 ) exit(1);
+   if( argc <= 2 ) exit(1);
 
-   if ( strlen(argv[1]) <= 15 ) {
+   int wrapLength = atoi(argv[2]);
+
+   /* if ( strlen(argv[1]) <= wrapLength ) {
       puts ( argv[1] );
       exit(0);
    }
+   */
 
+
+   char * lastDelim = NULL;
    char * input = strdup(argv[1]);
 
    char * current = NULL, * previous = NULL;
 
-   int chars = 0;
+   int chars = 1;
    int totalChars = 0;
 
    /* Find last word break with fewer than 80 chars */
 
    current = input;
    while( *current != '\0' ){
-      while ( chars <= 15 && *current != '\0'){
-         previous = current - 1;
+      while ( chars <= wrapLength && *current != '\0'){
          while ( !isDelim( *current ) ) ++current, ++chars;
-         if( *current != '\0' )
-            ++current;
-         else
-            goto done;
+         if( chars <= wrapLength) lastDelim = current;
+         current++, ++chars;
       }
-      *(previous) = '\n';
-      chars = 0;
+      *(lastDelim) = '\n';
+      current = lastDelim + 1;
+      chars = 1;
    }
 
 done:
    puts(input);
    free(input);
+
+   char * start = argv[1];
+   char * lastChar = argv[1];
+   current = argv[1];
+
+   while( *current != '\0' ){
+      while( 1 ){
+         while ( !isDelim( *current ) ) ++current, ++chars;
+         if( chars <= wrapLength){
+            lastChar = current-1; // lastDelim is actually the last non-delimiter
+            current++,chars++;
+         } else {
+            goto here;
+         }
+      }
+here:
+      printSection(start,lastChar);
+      putchar('\n');
+      current = lastChar + 1;
+      while(isDelim(*current))
+         if( *current == '\0') return 0; else ++current;
+      start = current;
+      lastChar = current;
+      chars = 1;
+   }
+
 
 
 

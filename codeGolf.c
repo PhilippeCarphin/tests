1f6d018bb4c5a0d6da7830801cb041a36073b8c7
Philippe Carphin <philippe.carphin@canada.ca>
2016-07-14 23:20:25 +0000

    All extraneous stuff removed. replaced goto's with while loops and made use of comma operator to save on brackets.

--- codeGolf.c
+++ codeGolf.c
@@ -1,25 +1,13 @@
-#define _GNU_SOURCE
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-
-int main ( int argc , char ** argv ) {
-
-   size_t N,n,m,i;float t,s,c,x;char*l=0;
-   getline(&l,&i,stdin);
-   sscanf(l,"%lu",&N);
-d:
-   getline(&l,&i,stdin);
-   sscanf(l,"%f",&x);
-   s=c=0;n=0;t=1;
-l:
-   m=n%2;
-   s += t*m;
-   c += t*!m;
-   t *= (m?-1:1)*x/++n;
-   if(n<60)goto l;
-   printf("%.3f\n%.3f\n",c,s);
-   if(--N)goto d;
-
-   return 0;
+int main(){
+   int N,n,m;
+   float t,s,c,x;
+   scanf("%d",&N);
+   while(N--){
+      scanf("%f",&x);
+      s=c=0;n=0;t=1;
+      while(n<60)
+         m=n%2,s+=t*m,c+=t*!m,t*=(m?-1:1)*x/++n;
+      printf("%.3f\n%.3f\n",s,c);
+   }
 }

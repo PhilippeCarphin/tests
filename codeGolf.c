b0318995431ac75e5ea8247f1ca60f0b10c6f399
Philippe Carphin <philippe.carphin@canada.ca>
2016-07-15 14:20:46 +0000

    Removed a couple characters

--- codeGolf.c
+++ codeGolf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 int main(){
    int N,n,m;
-   float t,s,c,x;
    scanf("%d",&N);
    while(N--){
+      float t=1,s=0,c=0,x;
       scanf("%f",&x);
-      s=c=0;n=0;t=1;
-      while(n<60)
+      for(n=0;n<60;)
          m=n%2,s+=t*m,c+=t*!m,t*=(m?-1:1)*x/++n;
       printf("%f\n%f\n",s,c);
    }

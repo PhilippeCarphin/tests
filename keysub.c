968e7f48e8bfab483965a520650539003116a345
Philippe Carphin <philippe.carphin@canada.ca>
2016-07-20 22:14:08 +0000

    Cleanup and comments

--- keysub.c
+++ keysub.c
@@ -4,67 +4,80 @@
 
 char * keysub(const char *str, const char ** values, const char *frontDelim, const char *endDelim)
 {
-   const char *p = str;
-   char retval[1000] = {'\0'};
-   char * dst = retval;
-   char variable[100];
-   char *var = variable;
-   const char *value;
-   const char *val;
-   int i = 0;
-   char *stopPoint = NULL;
+   char output[1000];   /* Char array in which we construct the output */
+   char variable[100];  /* Array into which we copy the variable name */
+   const char *p = str, /* Iterator on the characters of the input string */
+              *var,     /* Iterator on the characters of variable char array */
+              *value,   /* Value that will replace variable name */
+              *val;     /* Iterator on value of variable */
+   char * dst = output; /* Iterator on output array */
+   int i = 0;           /* Only used for testing */
+   char *stopPoint;     /* Used to locate occurrence of delimiter string */
 
    while( *p != 0 ){
-
+      /*
+       * Copy until the first occurrence of startDelim. If none is found, copy
+       * the whole string, and exit the loop.
+       */
       stopPoint = strstr(p,frontDelim);
       if(stopPoint != NULL){
-         while( p < stopPoint )
-            *dst++ = *p++;
+         while( p < stopPoint ) *dst++ = *p++;
       } else {
-         while(*p != 0)
-            *dst++ = *p++;
+         while(*p != 0) *dst++ = *p++;
          break;
       }
 
-      /* If *p == 0 we're done */
-      if( *p == '\0' )
-         break;
-
-      /* Now *p points on the start of the front delimiter string.  Check that
+      /*
+       * Now *p points on the start of the front delimiter string.  Check that
        * the delimiter string is valid, and advance by the length of said
-       * delimiter. */
-      if( p != strstr(p,frontDelim) ){
-         printf("ERROR:Incorect variable name front delimiter\n");
-         exit(1);
-      }
+       * delimiter. We know that we can safely advance by strlen(frontDelim)
+       * because we would have broken out of the loop if we couldn't
+       */
       p += strlen(frontDelim);
 
-      /* Copy into variable name until a endDelim is encountered*/
+      /*
+       * Copy until the first occurrence of endDelim.  We must be able to find
+       * endDelim or else it means that we got invalid input. The pointer p
+       * might point to the end of the string but we don't use it until the next
+       * iteration, so this case will be caught when we test to start the next
+       * iteration.
+       */
       stopPoint = strstr(p,endDelim);
       if(stopPoint != NULL){
-         while( p < stopPoint)
-            *var++ = *p++;
+         char *var = variable;
+         while( p < stopPoint) *var++ = *p++;
          p += strlen(endDelim);
-      }else{ 
-         printf("Front variable token %s must be accompanied by matching ending variable token %s\n", frontDelim, endDelim);
+      }else{
+         goto delim_mismatch;
       }
 
-      /* find value of variable */
-      /* Calling getdef, and have getdef either look in the deffile or in the
+      /*
+       * find value of variable
+       * Calling getdef, and have getdef either look in the deffile or in the
        * environment.  Everyone does his job, this functions job is to change
        * "${variable_name}" for "value".  And it's getdef's job to find the
-       * value corresponding to "variable_name". */
+       * value corresponding to "variable_name".
+       */
       value = values[i++];
 
-      /* Copy value into destination string */
+      /*
+       * Copy value into destination string
+       */
       val = value;
       while( *val != '\0' )
          *dst++ = *val++;
-
-      var = variable;
    }
+
+   /*
+    * We are done, mark the end of the return string with a nul byte
+    */
    *dst = '\0';
-   return strdup(retval);
+
+   return strdup(output);
+
+delim_mismatch:
+   printf("Front variable token %s must be accompanied by matching ending variable token %s\n",
+                                                         frontDelim, endDelim);
 }
 int main ( int argc , char ** argv ) {
 

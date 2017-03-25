2affb16b5f123a2f290f268be0963c7ebaea1af5
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-03-25 12:38:41 -0400

    Updated if.sh to reflect my new views

--- BASH_tests/if.sh
+++ BASH_tests/if.sh
@@ -14,3 +14,22 @@ if [[ -n $boner ]]; then
 else
 	echo "if échoué"
 fi
+
+# Now I prefer the following because it has the advantage of being easily
+# understandable.  No need to look up what -n does.
+
+variable=
+if [[ $variable == "" ]]; then
+	echo "variable is empty"
+else
+	echo "variable is not empty"
+fi
+
+echo "definition non nulle"
+variable=asdf
+
+if [[ $variable == "" ]]; then
+	echo "variable is empty"
+else
+	echo "variable is not empty"
+fi

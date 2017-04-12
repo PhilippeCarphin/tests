886a48dc0dacb56366ac06bcc8723cb14cbe7b40
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-04-12 15:41:22 -0400

    Comparison of switch and if-else-if structure

--- C_tests/switchStatement.c
+++ C_tests/switchStatement.c
@@ -1,22 +1,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-/* I made this to look at how the compiler creates assembler code for switch
- * statements */
+/*
+ * I made this to look at how the compiler creates assembler code for switch
+ * statements and compare it to an if-else structure
+ *
+ * The switch statement takes 68 lines of assembler code and the if takes 63
+ * lines.
+ *
+ * I have not done timing tests which would actually be much better than just
+ * counting assembler lines.
+ */
+
+
+/*******************************************************************************
+ * Silly switch statement based on input char
+*******************************************************************************/
 void what_char( char c ){
 
-   printf("HELLO\n");
    switch ( c )
    {
-      case 'a':
       case 'A':
          printf("A");
          break;
-      case 'b':
       case 'B':
          printf("B");
          break;
-      case 'c':
       case 'C':
          printf("C");
          break;
@@ -26,10 +35,31 @@ void what_char( char c ){
    printf("END\n");
 }
 
+/*******************************************************************************
+ * Silly if-else-if structur based on input char.
+*******************************************************************************/
+void what_char_if(char c)
+{
+	if(c == 'A'){
+		printf("A");
+	} else if (c == 'B'){
+		printf("B");
+	} else if (c == 'C'){
+		printf("C");
+	} else {
+		printf("Default");
+	}
+	printf("END\n");
+}
+
+
+
 int main ( int argc , char ** argv ) {
 
 
-   what_char('c');
-   what_char('a');
+   what_char('C');
+   what_char('A');
+   what_char_if('A');
+   what_char_if('C');
    return 0;
 }

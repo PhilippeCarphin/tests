77179068c52901df3b249188d9769ab9c24475e8
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-11-05 14:32:56 -0500

    Examining uninitialized memory returned by malloc

--- C_tests/initial_memory.c
+++ C_tests/initial_memory.c
@@ -7,8 +7,16 @@ int main(void)
 {
 	uint8_t *phil = malloc(256);
 	printf("strlen(phil) = %lu\n", strlen(phil));
+	int found = 0;
 	for(int i = 0; i < 1000; ++i){
 		printf("%d-", phil[i]);
+		if(phil[i] != 0){
+			++found;
+			if(found > 8){
+				putchar('\n');
+				break;
+			}
+		}
 		if(i % 16 == 0)
 			putchar('\n');
 	}

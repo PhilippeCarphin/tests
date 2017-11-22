b3408587b465f002f84e44763d51e64fdb2de771
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-11-22 01:37:54 -0500

    Cleaner of files generated by autoreconf and ./configure

---
+++ C_tests/autotools-test/phil_clean_autoconf.sh
@@ -0,0 +1,20 @@
+files="config.log
+ Makefile\
+ Makefile.in\
+ aclocal.m4\
+ autom4te.cache/\
+ compile\
+ config.h.in\
+ config.status\
+ configure\
+ depcomp\
+ install-sh\
+ missing\
+ src/.deps/\
+ src/Makefile\
+ src/Makefile.in"
+echo "$files"
+
+for f in $files ; do
+	rm -rf ./$f
+done

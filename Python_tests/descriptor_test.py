a309d7f5717228530497116538384887db1601cc
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-12-14 19:57:11 -0500

    added python descriptor test

--- Python_tests/descriptor_test.py
+++ Python_tests/descriptor_test.py
@@ -2,11 +2,10 @@ class Quantity:
     def __init__(self, sname):
         self.sname = sname
 
-    '''
     def __set__(self, instance, value):
+        ''' This method need not be created '''
         instance.__dict__[self.sname] = value
         print('descriptor class set method called')
-        '''
 
     def __get__(self, instance, other):
         ''' This method need not be created '''
@@ -14,9 +13,14 @@ class Quantity:
         return instance.__dict__[self.sname]
 
 class Item:
+    ''' Declare an instance of the Quantity descriptor class as a class
+    attribute of Item '''
     weight = Quantity('weight')
 
     def __init__(self, value):
+        ''' Now, assigning to an instance's self.weight will trigger the
+        descriptor.  The following line causes it's __set__ method to be called
+        if it exixts.'''
         self.weight = value
 
     def test(self):

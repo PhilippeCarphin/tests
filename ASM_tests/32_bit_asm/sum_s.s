20fe13adf3c22455b77aeac5be51fe0fd1f63e10
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-03-26 16:16:15 -0400

    Simplifications

--- ASM_tests/32_bit_asm/sum_s.s
+++ ASM_tests/32_bit_asm/sum_s.s
@@ -1,43 +1,12 @@
-	.globl	_sum_s
+	.globl	sum_s
 
-_sum_s:
+sum_s:
 
 	# Début de la fonction
 	pushl %ebp
 	movl %esp, %ebp
 
-	# Soustrait 8 a esp
-	pushl $0 # sum = 0 à -4(%ebp)
-	pushl $0 # i = 0 à -8(%ebp)
 
-
-check_continue:
-	movl -8(%ebp), %eax
-	cmpl 8(%ebp), %eax
-	jge done
-
-add_to_sum:
-	# eax = i + sum
-	movl -8(%ebp), %eax
-	addl -4(%ebp), %eax
-
-	# i + sum -> sum
-	movl %eax, -4(%ebp)
-
-	# i = i + 1;
-	movl -8(%ebp), %eax
-	addl $1, %eax
-	movl %eax, -8(%ebp)
-
-	jmp check_continue
-
-done:
-	# retour de la fonction
-	movl -4(%ebp), %eax
-
-	# cleanup du stack
-	addl $8, %esp
 	popl %ebp
-
 	# jump à l'instruction suivante
 	ret

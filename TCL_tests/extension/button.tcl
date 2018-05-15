9b4ab5ce83701cfae69a64aaa3aac5a567125e1c
Philippe Carphin <phil103@hotmail.com>
2018-05-15 19:45:52 -0400

    Added portability in name of extensions

--- TCL_tests/extension/button.tcl
+++ TCL_tests/extension/button.tcl
@@ -1,4 +1,4 @@
-#!/ssm/net/isst/maestro/1.4.3-rc4/linux26-x86-64/bin/wish
-load ./libhello.so
+#!/usr/bin/wish
+load ./libhello[info sharedlibextension]
 set external [button .asdf -text external -command {puts [hello]}]
 grid $external

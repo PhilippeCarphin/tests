cc9325d72e40afa4bb2f7295a02e639be4d0d8db
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-05-28 14:48:52 +0000

    Updated TCL C extension test

--- TCL_tests/extension/button.tcl
+++ TCL_tests/extension/button.tcl
@@ -1,4 +1,9 @@
 #!/ssm/net/isst/maestro/1.4.3-rc4/linux26-x86-64/bin/wish
 load ./libhello[info sharedlibextension]
-set external [button .asdf -text external -command {puts [hello bonjour 456]}]
+
+proc call_hello {} {
+   set result [hello bonjour 456]
+   puts "TCL : Result received from C extension : $result"
+}
+set external [button .asdf -text external -command {call_hello}]
 grid $external
--- TCL_tests/extension/sayhello.c
+++ TCL_tests/extension/sayhello.c
@@ -4,7 +4,7 @@
 static int print_args(int objc, Tcl_Obj *const objv[]){
     printf("C : %s() BEGIN\n", __func__);
     for(int i=0; i<objc; ++i){
-        puts(Tcl_GetString(objv[i]));
+        printf("C : Tcl_GesString(objv[%d])=%s\n",i, Tcl_GetString(objv[i]));
     }
     printf("C : %s() END\n", __func__);
 }
@@ -13,7 +13,7 @@ static int
 Hello_Cmd(ClientData cdata, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[])
 {
     printf("C : %s() BEGIN\n", __func__);
-    Tcl_SetObjResult(interp, Tcl_NewStringObj("Hello, World!", -1));
+    Tcl_SetObjResult(interp, Tcl_NewStringObj("HELLO_CMD RESULT", -1));
     print_args(objc, objv);
     printf("C : %s() END\n", __func__);
     return TCL_OK;

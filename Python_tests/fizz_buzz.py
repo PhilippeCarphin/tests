de72c5edcc97a13aad6c20d46fff42df553a99b6
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-06-12 17:46:28 -0400

    Added fizz-buzz function

--- Python_tests/fizz_buzz.py
+++ Python_tests/fizz_buzz.py
@@ -1,4 +1,5 @@
 def fizz_buzz(n):
+    """ This one checks an intermediate value """
     output = ''
     if n % 3 == 0:
         output += 'fizz'
@@ -8,6 +9,19 @@ def fizz_buzz(n):
         output = str(n)
     return output
 
+def fizz_buzz_2(n):
+    """ This one checks the same thing twice """
+    if n % 3 == 0:
+        if n % 5 == 0:
+            return 'fizzbuzz'
+        else:
+            return 'fizz'
+    else:
+        if n % 5 == 0:
+            return 'buzz'
+        else:
+            return str(n)
 
-for i in range(15):
-    print(fizz_buzz(i))
+
+for i in range(1,16):
+    print(fizz_buzz_2(i))

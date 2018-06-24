d68f3df828c1dafe7fcd34069ad35121d56a7db0
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-06-23 21:33:59 -0400

    Update modulo_power c_test

--- C_tests/modulo_powers_test.c
+++ C_tests/modulo_powers_test.c
@@ -2,15 +2,14 @@
 #include <stdlib.h>
 
 /*
- * This test shows the obsious, that integers have a maximum value in C and that
- * when we pass this value, an overflow occurs.  Therefore, in when calculating
- * the value of (2^311)%1000, for example, we must take the modulo at the same
- * time as we take powers since (a*b) == (a%n)(b%n) mod n;
+ * This test shows the obvious, that integers have a maximum value in C and that
+ * when we pass this value, an overflow occurs.  Therefore, when calculating the
+ * value of (2^311)%1000, for example, we must take the modulo at the same time
+ * as we take powers since (a*b) == (a%n)(b%n) mod n;
  *
  * There is also a python version of this experiment that shows that python can
- * haver arbitrarily large integers.
+ * have arbitrarily large integers.
  */
-
 
 /*******************************************************************************
  * This function gives an incorrect value since it calculates a^e, and then
@@ -32,7 +31,7 @@ unsigned long int exponentiate1(unsigned long int a, unsigned long int e, unsign
 
 /*******************************************************************************
  * This function gives correct results because overflow is avoided by taking the
- * remainder at each multiplication.  
+ * remainder at each multiplication.
 *******************************************************************************/
 unsigned long int exponentiate2(unsigned long int a, unsigned long int e, unsigned long int n)
 {
@@ -50,7 +49,7 @@ unsigned long int exponentiate2(unsigned long int a, unsigned long int e, unsign
 
 int main(void)
 {
-   printf("First  exp: 2^311 mod 1000 : %lu\n", exponentiate1(3,311,1000));
-   printf("Second exp: 2^311 mod 1000 : %lu\n", exponentiate2(3,311,1000));
+   printf("First  exp: 3^311 mod 1000 : %lu\n", exponentiate1(3,311,1000));
+   printf("Second exp: 3^311 mod 1000 : %lu\n", exponentiate2(3,311,1000));
    return 0;
 }

1b7bdee7ed6f72ef9b7d49cbd00cc62d4b53a232
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-08-06 21:42:27 +0000

    WIP : Experimenting with introspection

--- Python_tests/kwargs.py
+++ Python_tests/kwargs.py
@@ -12,12 +12,18 @@ def generate_cpp_opts(kwargs):
 def generate_cpp_command(funcname, argvalues):
     ARGS=0
     LOCALS=3
+    args=argvalues[ARGS]
+    values=argvalues[ARGS]
+    print(argvalues)
     kwargs = { arg: argvalues[LOCALS][arg] for arg in argvalues[ARGS]}
     return ' '.join([funcname, generate_cpp_opts(kwargs)]).strip()
 
 
-def named_arg_function(humeur, couleur):
-    cpp_cmd = get_caller_cpp_cmd()
+def named_arg_function(humeur=None, couleur='piss-bucket', phil_est_hot=True):
+    cpp_cmd = generate_cpp_command(
+        inspect.stack()[0][3],
+        inspect.getargvalues(inspect.currentframe())
+    )
     print("=="+cpp_cmd+"==")
 
 def get_caller_cpp_cmd():
@@ -56,10 +62,44 @@ def generate_python_functions(plugin_spec):
         functions += "\n\n" + func_def
     return functions
 
+def phildecorate(func):
+    funcname = func.__name__
+    argspec = inspect.getargspec(func)
+    argnames = argspec[0]
+    argdefaults = argspec[3]
+    defaults = dict(zip(argnames, argdefaults))
+    print("function name : " + funcname)
+    print("function argspec : " + str(argspec))
+
+    def new_func(**kwargs):
+        nonlocal defaults
+        for arg in kwargs:
+            default = defaults[arg]
+            if default == True or default == False:
+                print("{} is a flag".format(arg))
+            elif arg not in kwargs:
+                print("{} is not specified, use defautl = {}".format(arg, default))
+            else:
+                print("{} has value {}".format(arg, kwargs[arg]))
+    return new_func
+
+@phildecorate
+def decorated_arg_function(humeur=None, couleur='piss-bucket', phil_est_hot=True):
+    cpp_cmd = generate_cpp_command(
+        inspect.stack()[0][3],
+        inspect.getargvalues(inspect.currentframe())
+    )
+    print("=="+cpp_cmd+"==")
+
 
 if __name__ == "__main__":
     kwarg_function(humeur="joyeux", temperature="fa-chaud")
     kwarg_function(couleur="rose", vent="y-vente")
     named_arg_function(humeur=8, couleur=7)
     named_noarg_function()
+    print("CALLING DECORATED FUNCTION")
+    decorated_arg_function(humeur=None, couleur='piss-bucket', phil_est_hot=True)
+    print("CALLING DECORATED FUNCTION")
+    decorated_arg_function(humeur=None, phil_est_hot=True)
+    # phildecorate(named_arg_function)
     # print(generate_python_functions(plugin_spec))

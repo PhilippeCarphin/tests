8f8a568f8ea870ebb3f58c3068b3acbefd158f64
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-08-06 23:36:31 -0400

    Final implementation of the spooki introspection thing

--- Python_tests/kwargs.py
+++ Python_tests/kwargs.py
@@ -1,113 +1,129 @@
 import inspect
 
-def kwarg_function(**kwargs):
-    funcname = inspect.stack()[0][3]
-    print(funcname + ' ' + generate_cpp_opts(kwargs))
-
-def generate_cpp_opts(kwargs):
-    ''' generates a '--key value' string for each 'key: value' pair in
-    a dictionnary '''
-    return ' '.join([ "--{} {}".format(key, kwargs[key]) for key in kwargs])
-
-def generate_cpp_command(funcname, argvalues):
-    ARGS=0
-    LOCALS=3
-    args=argvalues[ARGS]
-    values=argvalues[ARGS]
-    print(argvalues)
-    kwargs = { arg: argvalues[LOCALS][arg] for arg in argvalues[ARGS]}
-    return ' '.join([funcname, generate_cpp_opts(kwargs)]).strip()
-
-
-def named_arg_function(humeur=None, couleur='piss-bucket', phil_est_hot=True):
-    cpp_cmd = generate_cpp_command(
-        inspect.stack()[0][3],
-        inspect.getargvalues(inspect.currentframe())
-    )
-    print("=="+cpp_cmd+"==")
-
-def get_caller_cpp_cmd():
-    caller_funcname = inspect.stack()[1][3]
-    caller_argvalues = inspect.getargvalues(inspect.currentframe().f_back)
-    kwargs = { arg: caller_argvalues[3][arg] for arg in caller_argvalues[0]}
-    return ' '.join([caller_funcname, generate_cpp_opts(kwargs)]).strip()
-
-def named_noarg_function():
-    cpp_cmd = generate_cpp_command(
-        inspect.stack()[0][3],
-        inspect.getargvalues(inspect.currentframe())
-    )
-    print("=="+cpp_cmd+"==")
-
-plugin_spec = {
-        'WindChill': ['param1', 'param2'],
-        'AbsoluteValue' : []
-        }
-
-def generate_function(plugin_name, argument_names):
-    func_decl = "def " + plugin_name + "(" + ', '.join(argument_names) + '):'
-    common_body = '''
-    cpp_cmd = generate_cpp_command(
-        inspect.stack()[0][3],
-        inspect.getargvalues(inspect.currentframe())
-    )
-    return libmeteo_operations.evaluatePlugin(cpp_cmd)
-'''
-    return func_decl + common_body
-
-def generate_python_functions(plugin_spec):
-    functions = ""
-    for plugin in plugin_spec:
-        func_def = generate_function(plugin, plugin_spec[plugin])
-        functions += "\n\n" + func_def
-    return functions
+class PySpookiImplementationError(Exception):
+    pass
 
 class PySpookiArgumentError(Exception):
     pass
 
-def phildecorate(func):
+def make_spooki_command(funcname, argnames, defaults, kwargs):
+    """Create a spooki command from a function call.
+
+    This function will replace the decorated function that specifies the plugin
+    interface.
+
+    ARGUMENTS
+
+    funcname : Name of the decorated function.  This name determines the spooki plugin.
+
+    argnames : Names of the arguments
+
+    defaults : Specifies the argument types and default values.
+           None: Specifies that the argument is mandatory
+           False: Specifies that the argument is a flag
+           Otherwise : Gives the default value of the argument
+
+    kwargs   : The keyword arguments from the function call.
+
+    EXCEPTIONS
+
+    PySpookiArgumentError :
+            arg has no value and default is None
+            arg is a flag but it's value is not True or False
+            arg has been given None value
+
+    NOTES
+
+    Note : I can make them not appear in the string sent to the C++ part, but I
+    don't think it's worth it to do.
+
+    """
+    def is_flag(arg):
+        return defaults[arg] is False
+    def is_specified(arg):
+        return arg in kwargs
+    def is_mandatory(arg):
+        return defaults[arg] is None
+    words = [funcname]
+    for arg in argnames:
+
+        if is_specified(arg):
+            argvalue = kwargs[arg]
+
+            if is_flag(arg):
+                if argvalue not in [True, False]:
+                    raise PySpookiArgumentError("Flag argument {} can only be True or False".format(arg))
+                if argvalue is True:
+                    words.append("--{}".format(arg))
+
+            else:
+                if kwargs[arg] is None:
+                    raise PySpookiArgumentError("Spooki functions cannot have None for arguments")
+                words.append("--{} {}".format(arg, kwargs[arg]))
+        else:
+            if is_mandatory(arg):
+                raise PySpookiArgumentError("Argument {} is required for".format(arg, funcname))
+            else:
+                words.append("--{} {}".format(arg, defaults[arg]))
+
+    return ' '.join(words)
+
+def make_spooki_function(func):
+    """Create a spooki function from a specification.
+
+    ARGUMENTS
+
+    func: A function that specifies an interface for a spooki plugin
+
+    RETURNS
+
+    new_func: A keyword argument function that construcst a spooki command and sends it to spooki
+
+    """
     funcname = func.__name__
     argspec = inspect.getargspec(func)
     argnames = argspec[0]
     argdefaults = argspec[3]
     defaults = dict(zip(argnames, argdefaults))
-    print("function name : " + funcname)
-    print("function argspec : " + str(argspec))
+    if len(argdefaults) != len(argnames):
+        raise PySpookiImplementationError("Spooki function {} must have default values for all arguments")
+    if True in argdefaults:
+        raise PySpookiImplementationError("Spooki function {} arguments cannot have a default value of True")
 
     def new_func(**kwargs):
-        words = [funcname]
-        for arg in argnames:
-            default = defaults[arg]
-            if default == False:
-                print("{} is a flag".format(arg))
-                if arg in kwargs and arg == True:
-                    words.append("--" + "arg")
-            elif arg not in kwargs:
-                if default is None:
-                    print("{} is not specified, use defautl = {}".format(arg, default))
-                    raise PySpookiArgumentError("Argument {} is required".format(arg))
-                words.append("--{} {}".format(arg, default))
-            elif arg in kwargs: 
-                if kwargs[arg] is None:
-                    raise PySpookiArgumentError("Spooki functions cannot have None values")
-                words.append("--{} {}".format(arg, kwargs[arg]))
-                print("{} has value {}".format(arg, kwargs[arg]))
-        print(' '.join(words))
+        spooky_cmd = make_spooki_command(funcname, argnames, defaults, kwargs)
+        print("libmeteo_information(\"[{}]\")".format(spooky_cmd))
     return new_func
 
-@phildecorate
-def decorated_arg_function(humeur=None, couleur='piss-bucket', phil_est_hot=True):
+@make_spooki_function
+def decorated_arg_function(humeur=None, couleur='piss-bucket', francois_est_hot=False):
+    """EXAMPLE SPECIFICATION : A function that doesn't do anything.
+
+    It is decorated to make a function that checks the argumensts, builds a
+    string and makes a call to the spooki plugin by the same name.
+
+    humeur is a mandatory argument, an exception will be raised if a call is
+    made without it.
+
+    couleur has a default value, it doesn't need to be specified.
+
+    phil_est_hot is a flag if it is set to true then the flag will be appended
+    to the command
+
+    """
+    pass
+
+@make_spooki_function
+def windchill(temperature="cold", pressure=130, some_flag=False):
     pass
 
 
 if __name__ == "__main__":
-    kwarg_function(humeur="joyeux", temperature="fa-chaud")
-    kwarg_function(couleur="rose", vent="y-vente")
-    named_arg_function(humeur=8, couleur=7)
-    named_noarg_function()
     print("CALLING DECORATED FUNCTION")
-    decorated_arg_function(humeur='piss-bucket', couleur='rouge', phil_est_hot=True)
+    decorated_arg_function(humeur='piss-bucket', couleur='rouge', francoisphil_est_hot=True)
     print("CALLING DECORATED FUNCTION")
     decorated_arg_function(humeur='Joyeux', phil_est_hot=True)
-    # phildecorate(named_arg_function)
-    # print(generate_python_functions(plugin_spec))
+    print("CALLING DECORATED FUNCTION")
+    decorated_arg_function(couleur='rouge', humeur='piss-bucket', francois_est_hot=True)
+    windchill()
+    windchill(some_flag=True)

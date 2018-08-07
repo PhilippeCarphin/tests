f68ad777c0d0043d3673ff2dd4e83543ec311e97
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-08-07 02:45:20 -0400

    Further cleanup

--- Python_tests/kwargs.py
+++ Python_tests/kwargs.py
@@ -1,12 +1,15 @@
 import inspect
 
+
 class PySpookiImplementationError(Exception):
     pass
+
 
 class PySpookiArgumentError(Exception):
     pass
 
-def make_spooki_command(funcname, argnames, defaults, kwargs):
+
+def make_spooki_command(funcname, argspec, kwargs):
     """Create a spooki command from a function call.
 
     This function will replace the decorated function that specifies the plugin
@@ -14,9 +17,8 @@ def make_spooki_command(funcname, argnames, defaults, kwargs):
 
     ARGUMENTS
 
-    funcname : Name of the decorated function.  This name determines the spooki plugin.
-
-    argnames : Names of the arguments
+    funcname : Name of the decorated function. This name determines the spooki
+    plugin.
 
     defaults : Specifies the argument types and default values.
            None: Specifies that the argument is mandatory
@@ -39,34 +41,35 @@ def make_spooki_command(funcname, argnames, defaults, kwargs):
 
     """
     def is_flag(arg):
-        return defaults[arg] is False
-    def is_specified(arg):
-        return arg in kwargs
-    def is_mandatory(arg):
-        return defaults[arg] is None
-    words = [funcname]
-    for arg in argnames:
+        return argspec[arg] is False
 
-        if is_specified(arg):
+    words = [funcname]
+    for arg in argspec:
+
+        if arg in kwargs:
             argvalue = kwargs[arg]
+            del kwargs[arg]
 
             if is_flag(arg):
                 if argvalue not in [True, False]:
                     raise PySpookiArgumentError("Flag argument {} can only be True or False".format(arg))
                 if argvalue is True:
                     words.append("--{}".format(arg))
-
+            elif argvalue is not None:
+                words.append("--{} {}".format(arg, argvalue))
             else:
-                if kwargs[arg] is None:
-                    raise PySpookiArgumentError("Spooki functions cannot have None for arguments")
-                words.append("--{} {}".format(arg, kwargs[arg]))
+                raise PySpookiArgumentError("Spooki functions cannot have None for arguments")
         else:
-            if is_mandatory(arg):
+            if argspec[arg] is None:
                 raise PySpookiArgumentError("Argument {} is required for".format(arg, funcname))
             else:
-                words.append("--{} {}".format(arg, defaults[arg]))
+                words.append("--{} {}".format(arg, argspec[arg]))
+
+    if kwargs:
+        raise PySpookiArgumentError("Arguments {} are not part of the specification".format(list(kwargs)))
 
     return ' '.join(words)
+
 
 def make_spooki_function(func):
     """Create a spooki function from a specification.
@@ -77,26 +80,31 @@ def make_spooki_function(func):
 
     RETURNS
 
-    new_func: A keyword argument function that construcst a spooki command and sends it to spooki
+    new_func: A keyword argument function that construcst a spooki command and
+              sends it to spooki.
 
     """
-    funcname = func.__name__
+    plugin_name = func.__name__
     argspec = inspect.getargspec(func)
     argnames = argspec[0]
     argdefaults = argspec[3]
-    defaults = dict(zip(argnames, argdefaults))
     if len(argdefaults) != len(argnames):
         raise PySpookiImplementationError("Spooki function {} must have default values for all arguments")
     if True in argdefaults:
         raise PySpookiImplementationError("Spooki function {} arguments cannot have a default value of True")
 
+    defaults = dict(zip(argnames, argdefaults))
+
     def new_func(**kwargs):
-        spooky_cmd = make_spooki_command(funcname, argnames, defaults, kwargs)
+        spooky_cmd = make_spooki_command(plugin_name, defaults, kwargs)
         print("libmeteo_information(\"[{}]\")".format(spooky_cmd))
     return new_func
 
+
 @make_spooki_function
-def decorated_arg_function(humeur=None, couleur='piss-bucket', francois_est_hot=False):
+def decorated_arg_function(humeur=None,
+                           couleur='piss-bucket',
+                           bonjour=False):
     """EXAMPLE SPECIFICATION : A function that doesn't do anything.
 
     It is decorated to make a function that checks the argumensts, builds a
@@ -107,7 +115,7 @@ def decorated_arg_function(humeur=None, couleur='piss-bucket', francois_est_hot=
 
     couleur has a default value, it doesn't need to be specified.
 
-    phil_est_hot is a flag if it is set to true then the flag will be appended
+    bonjour is a flag if it is set to true then the flag will be appended
     to the command
 
     """
@@ -120,10 +128,11 @@ def windchill(temperature="cold", pressure=130, some_flag=False):
 
 if __name__ == "__main__":
     print("CALLING DECORATED FUNCTION")
-    decorated_arg_function(humeur='piss-bucket', couleur='rouge', francoisphil_est_hot=True)
+    decorated_arg_function(humeur='piss-bucket', couleur='rouge', bonjour=True)
     print("CALLING DECORATED FUNCTION")
-    decorated_arg_function(humeur='Joyeux', phil_est_hot=True)
+    decorated_arg_function(humeur='Joyeux', bonjour=True)
     print("CALLING DECORATED FUNCTION")
-    decorated_arg_function(couleur='rouge', humeur='piss-bucket', francois_est_hot=True)
+    decorated_arg_function(couleur='rouge', humeur='piss-bucket', bonjour=True)
     windchill()
     windchill(some_flag=True)
+    windchill(not_a_real_arg=8)

115f106c9850f06d8e93ce4c4e22fc56bb3c323b
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-08-14 01:48:25 -0400

    WIP >>>>

--- Python_tests/decoraror_kwargs/spookiplugins.py
+++ Python_tests/decoraror_kwargs/spookiplugins.py
@@ -1,13 +1,26 @@
-from makespookifunction import make_spooki_function
-from spooki import IMO
-
+from pyunix import make_unix_function
 
 # noinspection PyUnusedLocal
-@make_spooki_function
+@make_unix_function
 def decorated_arg_function(humeur=None,
                            couleur=8,
                            bonjour=False) -> IMO:
     """EXAMPLE SPECIFICATION : A function that doesn't do anything.
+
+    # TODO The behavior will be that declaring a function to be 
+    or simpler than that.
+    a factory function that does this:
+    make_unix_function(initial_words, arg_spec):
+
+
+    git_clone('https://github.com/philippecarphin/configurations')
+    git_clone('https://github.com/philippecarphin/configurations', target='~/Documents/GitHub/config')
+
+
+    mv(src='somefile', 'dst=somefile')
+    mv(src='somefile', 'dst=somefile')
+
+    {target: NO_DEFAULT, link_name= NO_DEFAULT})
 
     It is decorated to make a function that checks the argumensts, builds a
     string and makes a call to the spooki plugin by the same name.
@@ -23,10 +36,33 @@ def decorated_arg_function(humeur=None,
     """
     pass
 
+def make_unix_function(initial_words, argspec=[()], kwargspec={}, options_before=True, varargs=false):
+    # Validate that argspec is a list of 2-element tuples
+    # validate that kwargspec is a dictionary of
+    # {
+    #     PyUnixKwarg(name=this, other_prop=that, option_prefix='--', with_equal=False, flag=False)
+    # }
+    # Both argspec and kwargspec will be lists of these. Optio
+    # Argspec same 'struct' but an ordered list of them instead of a dictionary of them.
+
+    if options_before:
+        def new_func(*args, **kwargs):
+            option_words = make_option_words(kwargspec, kwargs) # A list
+            arg_words = list(args)
+            command_words = initial_words + option_words + arg_words
+            subprocess.run(command_words)
+    else:
+        def new_func(*args, **kwargs):
+            option_words = make_option_words(kwargspec, kwargs) # A list
+            arg_words = list(args)
+            command_words = initial_words + arg_words + option_words
+            subprocess.run(command_words)
+
+
 
 # noinspection PyUnusedLocal
-@make_spooki_function
-def windchill(temperature="cold",
+@make_unix_function
+def git(temperature="cold",
               pressure=130,
               some_flag=False) -> IMO:
     """ This docstring will be given over to the new function """
@@ -34,7 +70,7 @@ def windchill(temperature="cold",
 
 
 # noinspection PyUnusedLocal
-@make_spooki_function
+@make_unix_function
 def annotated_func(humidity: int=8,
                    stickyness: str="hello",
                    sweatiness: str=None,

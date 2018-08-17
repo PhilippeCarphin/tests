bf2f007f442b09a4e0316ba19c86df7d87a55680
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-08-17 17:11:05 -0400

    Hello world in nodeJS

---
+++ NodeJS_tests/hello_world.js
@@ -0,0 +1,13 @@
+/*
+ * This test demonstrates the hello world program in Javascript using Node.
+ * To run it, you must install node with either
+ * $ brew install node
+ * or
+ * $ sudo dnf install node
+ */
+
+say_hello = function(name){
+    console.log("Hello " + name)
+}
+
+say_hello("Phil")

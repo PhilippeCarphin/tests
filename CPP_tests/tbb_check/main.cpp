bb06ccb3dbad5b9e0f1deabff2a3ef4a377275b3
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-11-15 12:57:35 -0500

    Factored out important code and placed at top of file

--- CPP_tests/tbb_check/main.cpp
+++ CPP_tests/tbb_check/main.cpp
@@ -7,6 +7,23 @@ void do_individual_element(size_t i, float f)
   std::cout << "array element " << i << " = " << f << std::endl;
 }
 
+void parallel_for_with_lambda(size_t n, float *my_floats)
+{
+  // On passe à parallel_for :
+  // - Un blocked_range que TBB va subdiviser et distribuer
+  // - Une fonction qui prend un blocked_range et fait du travail sur ce range
+  //   (en pratique ça sera une subdivision du range total faite automatiquement
+  //   par TBB.)
+parallel_for(tbb::blocked_range<size_t>(0,n),
+             [&my_floats](tbb::blocked_range<size_t> &r)
+             {
+               float *a = my_floats;
+               for(size_t i = r.begin(); i != r.end(); ++i){
+                 do_individual_element(i, a[i]);
+               }
+             });
+}
+
 
 /********************************************************************************
  * Name        : ArrayPrinter {
@@ -39,6 +56,11 @@ public:
   }
 };
 
+void parallel_for_with_object(size_t n, float *my_floats)
+{
+  parallel_for(tbb::blocked_range<size_t>(0,n), ArrayPrinter(my_floats));
+}
+
 int main(int argc, char *argv[])
 {
   std::cout << "TBB_INTERFACE_VERSION : " << TBB_INTERFACE_VERSION << std::endl
@@ -59,16 +81,9 @@ int main(int argc, char *argv[])
   };
   size_t n = sizeof(my_floats)/sizeof(float);
 
-  parallel_for(tbb::blocked_range<size_t>(0,n), ArrayPrinter(my_floats));
+  parallel_for_with_object(n, my_floats);
 
   std::cout << std::endl << "================================================================================" << std::endl << std::endl;
 
-  parallel_for(tbb::blocked_range<size_t>(0,n), [&my_floats](tbb::blocked_range<size_t> &r){
-                                                  float *a = my_floats;
-                                                  for(size_t i = r.begin(); i != r.end(); ++i){
-                                                    do_individual_element(i, a[i]);
-                                                  }
-                                                  return 1;
-                                                }
-    );
+  parallel_for_with_lambda(n, my_floats);
 }

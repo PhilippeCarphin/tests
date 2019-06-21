0bebd5bb66db399247dfd9f808cd71844e44cdda
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-06-21 14:07:10 -0400

    Boost logger with sinks

--- CPP_tests/boost_logging_sink/main.cpp
+++ CPP_tests/boost_logging_sink/main.cpp
@@ -2,8 +2,14 @@
 #include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
 void init()
 {
+    boost::log::add_file_log("sample.log");
     boost::log::core::get()->set_filter ( boost::log::trivial::severity >= boost::log::trivial::info );
 }
 int main(int, char*[])

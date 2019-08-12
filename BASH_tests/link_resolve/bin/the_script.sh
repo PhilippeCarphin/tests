94262e72f71defd83f596a96e7fbf04839ef38bc
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-08-12 16:48:34 -0400

    Playing with bash and python following of links

--- BASH_tests/link_resolve/bin/the_script.sh
+++ BASH_tests/link_resolve/bin/the_script.sh
@@ -12,7 +12,7 @@ portable_normpath(){
     echo "$(cd -P "$(dirname $1)" > /dev/null && pwd)/$(basename $1)"
 }
 
-follow_links()
+shell_follow_links()
 {
     local file="$(portable_normpath $1)"
     local curr_dir="$(dirname $file)"
@@ -27,3 +27,30 @@ follow_links()
     echo $file
 }
 
+py_follow_links()
+{
+    python -c "import os,sys; print(os.path.realpath(sys.argv[1]))" $1
+}
+
+follow_links(){
+    shell_follow_links $@
+}
+
+other_follow_links(){
+    target=$1
+
+    cd $(dirname $target)
+    target=$(basename $target)
+
+    while [ -L $target ] ;
+    do
+          target=$(readlink $target)
+          cd $(dirname $target)
+          target=$(basename $target)
+    done
+
+    dir=$(pwd -P)
+    file=$dir/$target
+
+    echo $file
+}

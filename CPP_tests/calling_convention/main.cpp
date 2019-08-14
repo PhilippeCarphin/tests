48a538b5a7805641fa0c2731b4eca3273f5d21e4
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-08-13 21:33:56 -0400

    No weirdness

--- CPP_tests/calling_convention/main.cpp
+++ CPP_tests/calling_convention/main.cpp
@@ -7,9 +7,9 @@ extern "C" int to_wrap_good(char *s, float f);
 
 extern "C" void f_ezsetval_(const char *, float f);
 
-extern "C" int c_ezsetval(const char *s, float f);
+extern "C" int c_ezsetval(const char *s, float *f);
 
-extern "C" int c_ezsetval2(const char *s, float * f);
+extern "C" int c_ezsetval2(const char *s, float *f);
 
 inline static void wrapper_function(std::string s, float f)
 {
@@ -30,10 +30,12 @@ int main(void)
 {
    std::string s = "The constant pi";
     float f = 3.1415;
-    // const char *c_str = s.c_str();
+    const char *c_str = s.c_str();
 
     // f_ezsetval_(c_str, f);
-    c_ezsetval(s.c_str(), f);
+    c_ezsetval(c_str, &f);
+    // c_ezsetval2(c_str, &f);
+    // c_ezsetval2()
 
     wrapper_function(s,f);
     wrapper_function_good(s,f);
--- CPP_tests/calling_convention/rmn.c
+++ CPP_tests/calling_convention/rmn.c
@@ -11,13 +11,13 @@ int to_wrap_good(char *s, float f)
         printf("The falue of the float is %f\n", f);
 }
 
-extern void f_ezsetval_(char * s, float *f);
-int c_ezsetval(char *s, float f)
+extern void f_ezsetval_(char * s, float *fp);
+int c_ezsetval(char *s, float *fp)
 {
-        f_ezsetval_(s, &f);
+        f_ezsetval_(s, fp);
 }
 
-int c_ezsetval2(char *s, float f)
+int c_ezsetval2(char *s, float *f)
 {
-        f_ezsetval_(s, &f);
+        f_ezsetval_(s, f);
 }

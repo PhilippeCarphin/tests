39d3833028d701ced5f87f532e7a8de3c4a86c49
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-10-06 10:14:16 -0400

    fork_test

--- C_tests/fork_test/main.c
+++ C_tests/fork_test/main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <unistd.h>
+int g_variable_int = 0;
 
 void task_of_subprocess(){
-    while(1) {
-        printf("subprocess");
-    }
+    printf("subprocess g_variable_int=%d\n", g_variable_int);
 }
+
+
 
 int main() {
 
     int fork_ret_val = fork();
     printf("Hello, World! fork_ret_val=%d\n", fork_ret_val);
+
+    if(fork_ret_val == 0){
+        printf("I am the parent process\n");
+    } else {
+        printf("I am the child process\n");
+        task_of_subprocess();
+    }
+
     return 0;
 }

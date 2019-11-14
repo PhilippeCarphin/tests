c1836059e197d309d15e5c5cb0cc77550dbfc76d
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-11-14 01:53:14 -0500

    Cleaned up python sourcing test

--- Python_tests/py-source/sourcing_guy.py
+++ Python_tests/py-source/sourcing_guy.py
@@ -3,20 +3,32 @@ import os
 import sys
 import subprocess
 
+def get_env_from_sourcing(filename):
+    stdout = subprocess.check_output(['/bin/bash', '-c', f'source {filename} >/dev/null 2>&1 ; env'])
+
+    new_env = {pair[0]: pair[1]
+               for pair in [
+                   line.split('=') for line in stdout.decode('utf-8').split('\n')
+               ] if len(pair) == 2
+               }
+
+    return new_env
 
 
-stdout = subprocess.check_output(['/bin/bash', '-c', 'source ./sourced_guy.sh >/dev/null 2>&1 ; env'])
+def pysource(filename):
+    new_env = get_env_from_sourcing(filename)
+    os.environ.update(new_env)
 
-new_env = {pair[0]: pair[1]
-           for pair in [
-               line.split('=') for line in stdout.decode('utf-8').split('\n')
-           ] if len(pair) == 2
-           }
 
-import pprint
+def setup_environment():
+    os.environ['SOURCING_GUY_EXPORTED_VARIABLE'] = 'SOURCING_GUY_VALUE'
+    pysource('./sourced_guy.sh')
 
-pprint.pprint(new_env)
+setup_environment()
+assert(os.environ['SOURCED_GUY_EXPORTED_VARIABLE'] != '')
 
-os.environ.update(new_env)
-
-os.execl('/bin/bash', '/bin/bash')
+# With this line, doing
+# python3 sourcing_guy.py
+# will make it look like you did
+# source sourced_guy.sh
+# os.execl('/bin/bash', '/bin/bash')

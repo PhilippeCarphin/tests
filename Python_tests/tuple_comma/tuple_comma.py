37e3ad03024c6dcc055e649fa1abd8a14d50a4f8
Philippe Carphin <philippe.carphin@polymtl.ca>
2020-02-05 22:25:11 -0500

    demo of why not put parens when you don't need to

---
+++ Python_tests/tuple_comma/tuple_comma.py
@@ -0,0 +1,13 @@
+symbol = None
+findNull = False
+validate = False
+count = 0
+if (symbol != None):
+    count += 1
+if (findNull == True,):
+    count += 1
+if (validate == True):
+    count += 1
+
+if count > 0:
+    print("WHAT")

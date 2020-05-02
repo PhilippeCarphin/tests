6fe4457329c28ad7e026ef7abfd46ea333c8872f
Philippe Carphin <philippe.carphin@polymtl.ca>
2020-05-02 19:54:48 -0400

    Cleanup

--- CPP_tests/variadic_templates/main.cpp
+++ CPP_tests/variadic_templates/main.cpp
@@ -3,58 +3,48 @@
 
 int main(void)
 {
-    int a[] = {1,2,3,4};
-    int b[] = {5,6,7,8};
-    int c[] = {9,10,11,12};
-    int N = 4;
-
-    std::cout << std::endl << "========= transform_phil ============" << std::endl;
-    auto f = [](int N, int *a, int *b, int *c){
+    constexpr int N = 4;
+    int a[N] = {1,2,3,4};
+    int b[N] = {5,6,7,8};
+    int c[N] = {0,0,0,0};
+    int d[N] = {0,0,0,0};
+    auto reset = [&](){for(int i = 0; i < N; i++){c[i] = 0; d[i] = 0;}};
+    auto check = [&](){
         for(int i = 0; i < N; i++){
-            c[i] = a[i] + b[i];
+            if(c[i] != a[i] + b[i]){
+                std::cout << "Invalid value for c[" << i << "]!!!" << std::endl;
+                exit(-1);
+            }
+            if(d[i] != a[i] + b[i] + c[i]){
+                std::cout << "Invalid value for d[" << i << "]!!!" << std::endl;
+                exit(-1);
+            }
         }
     };
 
-    transform_phil(f, 4, a, b, c);
-
-    for(int i = 0; i < N; i++){
-        std::cout << c[i] << " ";
-    }
-    int d[] = {0,0,0,0};
-    auto g = [](int N, int *a, int *b, int *c, int *d){
+    std::cout << "=========== variadic sum =============" << std::endl;
+    std::cout << "sum(1,2,3,4,5) : " << sum(1,2,3,4,5) << std::endl;
+    std::cout << std::endl << "========= transform_phil ============" << std::endl;
+    reset();
+    auto f = [](int N, int *a, int *b, int *c, int *d){
         for(int i = 0; i < N; i++){
+            c[i] = a[i] + b[i];
             d[i] = a[i] + b[i] + c[i];
         }
     };
-
-    transform_phil(g, N, a, b, c, d);
-    for(int i = 0; i < N; i++){
-        if(d[i] != a[i] + b[i] + c[i]){
-            std::cout << "Invalid value for d[" << i << "]!!!" << std::endl;
-            return -1;
-        }
-    }
-    std::cout << "Success calling g()" << g << std::endl;
+    transform_phil(f, N, a, b, c, d);
+    check();
+    std::cout << "Success with transform_phil" << std::endl;
 
     std::cout << std::endl << "======= transform_paul ==============" << std::endl;
-    for(int i = 0; i < N; i++){c[i] = 0; d[i] = 0;}
+    reset();
     auto h = [](int N, int *a, int *b, int *c, int *d){
         *c = *a + *b;
         *d = *a + *b + *c;
     };
     transform_paul(h, N, a, b, c, d);
-    for(int i = 0; i < N; i++){
-        if(c[i] != a[i] + b[i]){
-            std::cout << "Invalid value for c[" << i << "]!!!" << std::endl;
-            return -1;
-        }
-        if(d[i] != a[i] + b[i] + c[i]){
-            std::cout << "Invalid value for d[" << i << "]!!!" << std::endl;
-            return -1;
-        }
-    }
+    check();
     std::cout << "Success with transform_paul" << std::endl;
 
-    std::cout << "sum(1,2,3,4,5) : " << sum(1,2,3,4,5) << std::endl;
     return 0;
 }

f9b45cdae432befabec3bc535b8f87433ba40ce5
Philippe Carphin <philippe.carphin@polymtl.ca>
2020-07-12 15:22:14 -0400

    Make independant of PWD, add exception for CTRL-D

--- Python_tests/pipe_demo/pipe_demo.py
+++ Python_tests/pipe_demo/pipe_demo.py
@@ -5,7 +5,7 @@ import time
 
 #Kinda sourceing a script
 def sourcing_demo():
-    script = './sourced_guy.sh'
+    script = os.path.join(os.path.dirname(__file__), 'sourced_guy.sh')
     q = subprocess.Popen(['bash'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     with open(script, 'r') as s:
         script_text = s.read()
@@ -17,13 +17,16 @@ def sourcing_demo():
 
 def process_wrapper_demo():
     p = subprocess.Popen(['bash'], stdin=subprocess.PIPE)
-    while True:
-        host = os.uname().nodename
-        user = os.environ['USER']
-        cmd = input(f'{user}@{host} {os.getcwd()} $ ')
-        p.stdin.write(cmd.encode('utf-8') + b'\n')
-        p.stdin.flush()
-        time.sleep(0.1)
+    try:
+        while True:
+            host = os.uname().nodename
+            user = os.environ['USER']
+            cmd = input(f'{user}@{host} {os.getcwd()} $ ')
+            p.stdin.write(cmd.encode('utf-8') + b'\n')
+            p.stdin.flush()
+            time.sleep(0.1)
+    except EOFError:
+        pass
     # If I wanted my script to have access to stdout
     # The sleep is like making sure that we empty stdout before printing the prompt because we don't read stdout
     # You need a separate thread that reads things and puts them in a queue

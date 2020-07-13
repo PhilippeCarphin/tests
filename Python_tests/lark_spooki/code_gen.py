70aef4541e2ff76e583b66157a4a2dd4f71633d3
Philippe Carphin <philippe.carphin@polymtl.ca>
2020-07-12 21:04:02 -0400

    Bob Martin refactoring

--- Python_tests/lark_spooki/code_gen.py
+++ Python_tests/lark_spooki/code_gen.py
@@ -12,43 +12,58 @@ def display_default(default):
     else:
         return default
 
-def print_opt(opt):
+def print_name_param(opt):
+    print(f"        \"{opt['name']}\" \"{opt['sname']}\",")
+
+def print_value_param(opt):
     opt_type = opt['type']
-    print('(')
-    print(f"    \"{opt['name']}\" \"{opt['sname']}\",")
     if opt['has_default']:
         default = opt['default_value']
-        print(f"    po::value<{opt_type}>()->default_value({default},{display_default(default)}),")
+        print(f"        po::value<{opt_type}>()->default_value({default},{display_default(default)}),")
     else:
-        print(f"    po::value<{opt_type}>(),")
+        print(f"        po::value<{opt_type}>(),")
+
+def print_help_param(opt):
     help = opt['help'].replace('\n', '\\n')
     if opt['mandatory']:
-        print(f"    \"*    {help}\"")
+        print(f"        \"*    {help}\"")
     else:
-        print(f"    \"     {help}\"")
-    print(')')
+        print(f"        \"     {help}\"")
+
+def print_opt(opt):
+    print('    (')
+    print_name_param(opt)
+    print_value_param(opt)
+    print_help_param(opt)
+    print('    )')
 
 def print_options(options):
-    print('OperationBase::addSpecificOptions()')
+
+    print('void addBasicOptions() {')
+    print('    HighLevelOperation::addBasicOptions();')
+    print('    OperationBase::addSpecificOptions()')
     for opt in options:
         print_opt(opt)
-    print(';')
+    print('    ;')
+    print('}')
 
-s = "AIR_DENSITY::ACTUAL".split('::')
-print(s)
+def declare_and_set(opt):
+    get_opt_value = f"getParameterValue(\"{opt['name']}\", {opt['name']});"
+    if opt['mandatory']:
+        print(f"{opt['type']} {opt['name']};")
+        print(get_opt_value)
+    else:
+        print(f"{opt['type']} {opt['name']} = {opt['type']}();")
+        print("if(parameterExists(\"{opt['name']}\")){")
+        print("    " + get_opt_value)
+        print("}")
 
 def get_parameter_values(options):
     for opt in options:
-        get_opt_value = f"getParameterValue(\"{opt['name']}\", {opt['name']});"
-        if opt['mandatory']:
-            print(f"{opt['type']} {opt['name']};")
-            print(get_opt_value)
-        else:
-            print(f"{opt['type']} {opt['name']} = {opt['type']}();")
-            print("if(parameterExists(\"{opt['name']}\")){")
-            print("    " + get_opt_value)
-            print("}")
+        declare_and_set(opt)
         
 
+print("--------- PROGRAM OPTIONS --------------------------")
 print_options(options)
+print("--------- GET_PARAMETER_VALUES() -------------------")
 get_parameter_values(options)

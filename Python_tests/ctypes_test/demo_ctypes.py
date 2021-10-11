ce1d01cf392f8d67afee70d502e07e74acd69d9e
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-10-11 06:07:35 -0400

    Update ctypes demo

--- Python_tests/ctypes_test/demo_ctypes.py
+++ Python_tests/ctypes_test/demo_ctypes.py
@@ -2,21 +2,38 @@
 import ctypes
 import os
 
+# 1. Obtain an object for your shared library
+# 2. Create wrapper functions for C library functions
+#    2.1 C object wrapping a library function(s)
+#    2.2 Configure the object according to the function signature(s)
+#    2.3 Create a wraper function that takes and returns Python types
+# (*) Note: If you have access to the C code being exposed, then
+#           adding C functions to help with the python wrapping is
+#           always possible
+
+# 1 ############################################################################
 libc_name = "libc.so.6"
 if os.uname().sysname == 'Darwin':
     libc_name = "libc.dylib"
 
 libc = ctypes.cdll.LoadLibrary(libc_name)
 
-# This line looks for a function called getenv.
+# 2 ############################################################################
+# 2.1 ##########################################################################
+# This line looks for a function called getenv. (This is done by overloading
+# the __getattr__ method.
 getenv = libc.getenv
-# The result is a _FuncPtr object that encapsulates the C function
-print(f'getenv = {getenv}')
 
+# We now have a function that we can call!
+home = getenv(b'HOME')
+oopsie = getenv('HOME')
+print(f'home = {home}')
+print(f'oopsie = {oopsie}')
+
+# 2.2 ##########################################################################
 # We need to tell this encapsulator what the functions signature is
 getenv.restype = ctypes.c_char_p
 getenv.argtypes = [ctypes.c_char_p]
-
 
 # c_char_p represents a byte array, we have to give the function a bytes object
 home = getenv(b'HOME')
@@ -24,6 +41,7 @@ home = getenv(b'HOME')
 print(f'type(home) = {type(home)}')
 print(home)
 
+# 2.3 ##########################################################################
 # Once you have access to your C functions they can be further wrapped to
 # completely hide that there is a C function under the hood:
 def pygetenv(var):
@@ -33,15 +51,14 @@ def pygetenv(var):
 pyhome = pygetenv('HOME')
 print(pyhome)
 
-# Demo ends here, the rest is fiddling around with types.
+# Demo ends here, the rest is fiddling around with types. ######################
 quit()
 
 
-#
+
 # The default return type will be c_int in which case, our return value will
 # lose 32 of its 64 bits on a machine where the sizeof(int) = 32 and
 # sizeof(char*) = 64
-#
 getenv.restype = ctypes.c_uint
 res32 = getenv(b'HOME')
 getenv.restype = ctypes.c_longlong
@@ -51,8 +68,7 @@ print(f'{" "*32}res32 = {res32:032b}')
 print(f'res64 = {res64:064b}')
 print(f'res32 = {res32:064b}')
 
-#
+
 # The 64 bit int can be turned into a ctypes.c_char_p and read as a string
-#
 res64_char_p = ctypes.c_char_p(res64)
 print(f'res64_char_p.value = {res64_char_p.value}')

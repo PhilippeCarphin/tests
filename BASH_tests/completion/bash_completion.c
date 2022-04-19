dfc8061c86c02285d42f1a8c74e391c319df7a97
Philippe Carphin <philippe.carphin@polymtl.ca>
2022-04-19 19:23:50 -0400

    Added C program to demonstrate interaction with binaries

---
+++ BASH_tests/completion/bash_completion.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv){
+    char *s = getenv("HOME");
+    char *f = "log.txt";
+    int len = strlen(s) + strlen(f) + 2; // (2 : for '/' and '\0')
+    char *filepath = malloc((len+1)*sizeof(*filepath));
+
+    int res = snprintf(filepath, len+1, "%s/%s", s, f);
+    FILE *log = fopen(filepath, "a+");
+
+    if(res == len){
+        fprintf(log, "Error composing path\n");
+    }
+
+    for(int i = 0; i < argc; i++){
+        fprintf(log, "argv[%d] = '%s'\n", i, argv[i]);
+    }
+
+    fprintf(log, "COMP_LINE = '%s'\n", getenv("COMP_LINE"));
+    fprintf(log, "COMP_POINT = '%s'\n", getenv("COMP_POINT"));
+    fprintf(log, "COMP_KEY = '%s'\n", getenv("COMP_KEY"));
+    fprintf(log, "COMP_TYPE = '%s'\n", getenv("COMP_TYPE"));
+
+    printf("apple\napricot\nbananna\ncherry\n");
+
+    return 0;
+}

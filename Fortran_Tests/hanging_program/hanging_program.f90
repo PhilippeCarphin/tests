6822dbe21ec66135b78236525182fa5f579d6ace
Philippe Carphin <philippe.carphin@polymtl.ca>
2022-06-29 17:08:07 -0400

    Add explanation of Fortran hanging_program.f90 after having asked at work

--- Fortran_Tests/hanging_program/hanging_program.f90
+++ Fortran_Tests/hanging_program/hanging_program.f90
@@ -5,6 +5,17 @@
 !
 ! I have tried it on the linux computers at work and it never hangs.
 !
+! The problem, according to a Fortran expert at work is that we are doing
+! IO while there is already an IO operation under way.
+!
+! During the IO operation in the program, we call the function which
+! itself contains an IO operation.
+!
+! Te problem does not happen if in the program we do
+! integer :: pi
+! pi = program_module_func()
+! write (*,*) "pi = ", pi
+!
 module mainmod
     contains
         real function program_module_func()

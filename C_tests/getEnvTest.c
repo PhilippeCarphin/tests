b31933995e405e283d91a54a423bd9e57c49eef3
Philippe Carphin <philippe.carphin@polymtl.ca>
2022-07-14 22:31:04 -0400

    Cleanup C getenv test

--- C_tests/getEnvTest.c
+++ C_tests/getEnvTest.c
@@ -5,14 +5,19 @@
 int main( int argc, char** argv)
 {
    char* myVar = getenv("VAR");
-   char* secondVar = strdup(getenv("VAR"));
-   printf("getenv()        : %s\n", getenv("VAR"));
-   printf("getenv() addr   : %.16p\n",getenv("VAR"));
-   printf("myVar string    : %s\n",myVar);
-   printf("myVar addr      : %.16p\n",myVar);
-   myVar = strdup(myVar); 
-   printf("myVar string    : %s\n",myVar);
-   printf("myVar addr      : %.16p\n",myVar);
-   printf("secondVar string: %s\n",secondVar);
-   printf("secondVar addr  : %.16p\n",secondVar);
+   printf("getenv(\"VAR\")        : %s\n", getenv("VAR"));
+   printf("getenv(\"VAR\") addr   : %.16p\n",getenv("VAR"));
+   printf("myVar string         : %s\n",myVar);
+   printf("myVar addr           : %.16p\n",myVar);
+
+   if(myVar == NULL){
+       printf("Cannot strdup(NULL)\n");
+       return 0;
+   }
+   char *secondVar = strdup(myVar);
+
+   printf("secondVar string     : %s\n",secondVar);
+   printf("secondVar addr       : %.16p\n",secondVar);
+
+   return 0;
 }

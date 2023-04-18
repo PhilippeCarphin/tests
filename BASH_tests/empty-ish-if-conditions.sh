dae80920bb8f98033b96954eb97556043111ae08
Philippe Carphin <philippe.carphin@ec.gc.ca>
2023-04-18 17:05:33 -0400

    Added more text in empty-ish-if-condition.sh

--- BASH_tests/empty-ish-if-conditions.sh
+++ BASH_tests/empty-ish-if-conditions.sh
@@ -1,4 +1,15 @@
+#
+# This demonstrates the weird behavior of 'if' in BASH when the condition
+# is some kind of thing that works out to an empty if.
+#
+# The last three are not surprising: they cause errors.
+#
+# What is surprising is that the first three, which after varialble expansion,
+# are all like the very last one, produce no error message and go inside the
+# IF.
+#
 
+# Goes in the IF, no error message
 echo "=== DOESNT_EXIST"
 if $DOESNT_EXIST ; then
     echo "IN THE IF"
@@ -6,14 +17,7 @@ else
     echo "IN THE ELSE"
 fi
 
-echo "=== EMPTY_STRING_VAR"
-EMPTY_STRING_VAR=""
-if $EMPTY_STRING_VAR ; then
-    echo "IN THE IF"
-else
-    echo "IN THE ELSE"
-fi
-
+# Goes in the IF, no error message
 echo "=== EMPTY_VAR"
 EMPTY_VAR=
 if $EMPTY_VAR ; then
@@ -22,6 +26,25 @@ else
     echo "IN THE ELSE"
 fi
 
+# Goes in the IF, no error message
+echo "=== EMPTY_STRING_VAR"
+EMPTY_STRING_VAR=""
+if $EMPTY_STRING_VAR ; then
+    echo "IN THE IF"
+else
+    echo "IN THE ELSE"
+fi
+
+# Goes in the ELSE, Error message that command '' is not found
+echo "=== quoted EMPTY_STRING_VAR"
+EMPTY_STRING_VAR=""
+if "$EMPTY_STRING_VAR" ; then
+    echo "IN THE IF"
+else
+    echo "IN THE ELSE"
+fi
+
+# Goes in the ELSE, Error message that command '' is not found
 echo "=== EMPTY STRING LITTERAL"
 if "" ; then
     echo "IN THE IF"
@@ -29,6 +52,8 @@ else
     echo "IN THE ELSE"
 fi
 
+# Does not go in the IF or the ELSE because it is a syntax error
+# and it stops the program.
 echo "=== Completely empty if"
 if  ; then
     echo "IN THE IF"

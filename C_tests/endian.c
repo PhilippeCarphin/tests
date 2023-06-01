544bf83ffe6c9b1b8d4d534c8b6d2ec21c27e417
Philippe Carphin <philippe.carphin@ec.gc.ca>
2023-06-01 13:23:06 -0400

    Update Endianness test

--- C_tests/endian.c
+++ C_tests/endian.c
@@ -4,10 +4,11 @@
 
 int main(void)
 {
-	int a = 0xAABBCCDD;
+	unsigned int a = 0xAABBCCDD;
+
 	unsigned char *bytes = (unsigned char *)&a;
-   printf("The bytes in memory of %x are\n", a);
-	for(unsigned int i = 0; i < sizeof(int); ++i){
+
+	for(unsigned int i = 0; i < sizeof(a); ++i){
 		printf("bytes[%d] = %x\n", i, bytes[i]);
 	}
 	if(bytes[0] == 0xDD)
@@ -15,5 +16,7 @@ int main(void)
 	else
 		printf("Big-endian\n");
 
+    printf("a << 8 = %08x\n", (a << 8));
+    printf("a >> 8 = %08x\n", (a >> 8));
 	return 0;
 }

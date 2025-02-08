d2be6684df805fb680fd650e2d01463b7251fab8
Philippe Carphin <phil103@hotmail.com>
2025-02-07 20:34:19 -0500

    Have the bug manifest to use as an example for GDB

--- C_tests/array_diff_pointer/simple_demo/main.c
+++ C_tests/array_diff_pointer/simple_demo/main.c
@@ -7,6 +7,9 @@ int main(void)
 {
     printf("The value of the pointer glb_arr is %p\n", glb_arr);
     array_func();
+    // Attempt to get the first element of the array
+    int first_elem = glb_arr[0];
 
+    printf("The value of the first element is %d\n", first_elem);
     return 0;
 }

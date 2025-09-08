81380a9ddcce2de8d04fb4bf044a6e129571fda5
Philippe Carphin <phil103@hotmail.com>
2025-09-08 19:57:11 -0400

    Add explanation in comments

--- C_tests/sscanf_failure_return/main.c
+++ C_tests/sscanf_failure_return/main.c
@@ -11,15 +11,32 @@ int main(int argc, char **argv)
     // The manpage `man 3 sscanf` says
     // > The value EOF is returned if the end of input is reached before either
     // > the first successful conversion or a matching failure occurs.
-    // but doing `sscanf(argv[1], "%d", &number) == EOF` is not true when we
+    // but doing `sscanf("hello", "%d", &number) == EOF` is not true when we
     // pass say 'asdf' as the argument.  However to me it looks like we reach
     // the end of input before the first successful conversion so sscanf should
     // return EOF in that case.
-    if(sscanf(argv[1], "%d", &number) != 1){
+    int sscanf_result = sscanf(argv[1], "%d", &number);
+    fprintf(stderr, "sscanf_result=%d (btw EOF=%d)\n", sscanf_result, EOF);
+    if(sscanf("hello", "%d", &number) != 1){
         fprintf(stderr, "Conversion error: %s\n", argv[1]);
-        return 1;
+        // return 1;
     }
     fprintf(stderr, "Your number is %d\n", number);
+
+    // OK, I can get it, it is meant to be read as
+    // > ... end of input is reached before (the first successful confversion
+    // > or a matching failure) occurs
+    // rather than being interpreted as
+    // > ... end of input is reached ((before the first successful conversion) or
+    // > ([when] a matching failure occurs))
+    // Which is also illogical if we don't add 'when':
+    // > ... end of input is reached before the first successful conversion
+    // > or end of input is reached a matching failure occurs
+    // so if a matching failure occurs, the function *will not* return EOF, but
+    // if we pass the empty string as the first argument, we reach the end of
+    // input before the first successful match *AND* before a matching failure
+    // occurs.
+
 
     const unsigned char x = number;
     const size_t nbits = 8 * sizeof(x);

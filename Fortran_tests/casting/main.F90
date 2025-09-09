75c60ecd483b688d45373f7286b69893e76ab788
Philippe Carphin <phil103@hotmail.com>
2025-09-08 20:07:34 -0400

    Improve casting test

--- Fortran_tests/casting/main.F90
+++ Fortran_tests/casting/main.F90
@@ -13,6 +13,13 @@ module mainmod
             times_three = 3*ci
         end function
 
+        integer(kind=C_INT) function double_in_place(cl)
+            integer(kind=C_LONG), intent(inout) :: cl
+            cl = 2*cl
+            double_in_place = 0
+        end function
+
+
 end module
 
 program main
@@ -20,15 +27,26 @@ program main
     use, intrinsic :: iso_c_binding
     use, intrinsic :: iso_fortran_env
     use mainmod
+    implicit none
+    integer(kind=C_INT) :: two_hundred_and_fifty_eight
     integer(kind=C_INT) :: forty_three = 43
     integer(kind=C_INT) :: eighty_six
+    integer(kind=C_INT) :: ier
 
-    eighty_six = times_two(forty_three)
+    ! Error (or maybe warning with some compilers): Wront type of argument
+    !       passing INTEGER(4) to INTEGER(8)
+    ! eighty_six = times_two(forty_three)
 
     eighty_six = times_two(int(forty_three, kind=C_LONG))
 
     two_hundred_and_fifty_eight = times_three(eighty_six)
 
     write(error_unit, *) "258 = ", two_hundred_and_fifty_eight
+
+    ! Error Passing non-variable expression in variable definition context
+    !       (actual argument to INTENT = OUT/INOUT)
+    !       >>> Safe to use int(x,kind=Y) as the argument of a function because
+    !           the compiler won't let us do it if it's to an OUT/INOUT arg
+    ! ier = double_in_place(int(forty_three, kind=C_LONG))
 
 end program 

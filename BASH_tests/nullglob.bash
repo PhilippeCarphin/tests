fe20206ef43f56e68a6bf4fb9faf2f2e9cc2cab2
Philippe Carphin <phil103@hotmail.com>
2026-02-14 21:08:55 -0500

    Add text explanation

--- BASH_tests/nullglob.bash
+++ BASH_tests/nullglob.bash
@@ -1,7 +1,15 @@
+# in tui-selector, I use bash_normpath and it stopped working when I had
+# shopt -s nullglob.  It tooke me a while to figure out that was the difference
+# but it turned out to be because the `new_tokens[i]` in `unset new_tokens[i]`
+# is a glob expression that stays as is when `shopt -u nullglob` because it's
+# quite unlikely I would have a file named `new_tokensi` in my current directory
+# but with nullglob, `new_tokens[i]` expands to nothing! so the line becomes
+# just `unset`
+#
+i=${1:-3}
 shopt -p extglob # Does not make a difference
 echo "======== Without nullglob =================="
 array=(A B C D E F)
-i=${1:-3}
 echo "array[i] glob expands to: " array[i]
 unset array[i]
 declare -p array
@@ -9,7 +17,6 @@ declare -p array
 echo "======== With nullglob =================="
 shopt -s nullglob
 array=(A B C D E F)
-i=${1:-3}
 echo "array[i] glob expands to: " array[i]
 unset array[i]
 declare -p array
@@ -18,7 +25,6 @@ echo "======== With nullglob and quotes =================="
 shopt -s nullglob
 echo "\"array[i]\" glob expands to: " "array[i]"
 array=(A B C D E F)
-i=${1:-3}
 unset 'array[i]'
 declare -p array
 
@@ -27,7 +33,6 @@ touch arrayi
 echo "Created file arrayi"
 echo "======== (WITH MATCH) Without nullglob =================="
 array=(A B C D E F)
-i=${1:-3}
 echo "array[i] glob expands to: " array[i]
 unset array[i]
 declare -p array
@@ -35,7 +40,6 @@ declare -p array
 echo "======== (WITH MATCH) With nullglob =================="
 shopt -s nullglob
 array=(A B C D E F)
-i=${1:-3}
 echo "array[i] glob expands to: " array[i]
 unset array[i]
 declare -p array
@@ -44,8 +48,6 @@ echo "======== (WITH MATCH) With nullglob and quotes =================="
 shopt -s nullglob
 echo "\"array[i]\" glob expands to: " "array[i]"
 array=(A B C D E F)
-i=${1:-3}
 unset 'array[i]'
 declare -p array
-# Note array[i] as a glob would match the file arrayi so retry 
 rm -v arrayi
